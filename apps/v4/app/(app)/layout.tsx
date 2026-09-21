a9129849cb53969e1ca05397c0f40bd668100325
Nitesh000 <ntudu040@gmail.com>
2026-09-21 19:35:48 +0530

    feat: announcement above top bar

--- apps/v4/app/(app)/layout.tsx
+++ apps/v4/app/(app)/layout.tsx
@@ -7,8 +7,13 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       data-slot="layout"
       className="group/layout relative z-10 flex min-h-svh flex-col bg-background has-data-[slot=designer]:h-svh has-data-[slot=designer]:overflow-hidden"
     >
+      <div className="flex justify-center items-center p-2 bg-card">
+        <p className="font-mono tracking-wider">
+          The product currently in development phase.
+        </p>
+      </div>
       <SiteHeader />
-      <main className="flex min-h-0 flex-1 flex-col">{children}</main>
+      <main className="flex flex-col flex-1 min-h-0">{children}</main>
       <SiteFooter />
     </div>
   )
